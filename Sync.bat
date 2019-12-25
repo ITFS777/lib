23cd3fbcb0acfe65652401d342a72dba44b7cd08
ITFS <1374924410@qqcom>
2019-12-25 22:45:18 +0800

    2019/12/25 22:45:18 by ITFS<Aryan.ITFS@Gmail.com>

--- Sync.bat
+++ Sync.bat
@@ -1,4 +1,4 @@
 @echo off
 git add .
-git commit -m "%date:~3,10% %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git commit -m "%date:~3,10% %time:~0,2%:%time:~3,2%:%time:~6,2% by ITFS<Aryan.ITFS@Gmail.com>"
 git push lib master
