e8f5f2e3d06975385f543da037bacdc2408f5da0
Aryan Sun <Aryan.ITFS@iCloud.com>
2020-08-21 15:19:15 +0800

    2020/08/21 15:19:15 <Aryan.ITFS@iCloud.com>

--- C&C++/clib.h
+++ C&C++/clib.h
@@ -4,3 +4,4 @@ void insertStr(char *string_1, char *string_2, char ch);
 int decCom(int array_dec_3_3);
 bool isPrime(long long int num);
 void *hash(const char *name, const void *base, int size, const char *hash_salt);
+std::string getPasswd(void);
